6f5bd86375a9d296fe1ab2e3db99f0f4c3fe3412
dliu <divid86391@hotmail.com>
2015-05-24 10:34:59 -0700

    Start using backbone structure

--- js/test.js
+++ js/test.js
@@ -1,8 +1,2 @@
 // This is a test js file
-
-// To set the text for a paragraph
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
-// To set the behabior for button
-document.getElementById("b1").onclick = function() {
-  alert('test alert msg!');
-};
+$('h1').html('New demo page!');
